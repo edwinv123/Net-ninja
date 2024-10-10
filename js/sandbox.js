60e5f1be5b47ba866f87beca486aebc2cd7de4f0
edwinv123 <126evalentinnunez@frhsd.com>
2024-10-10 12:49:52 -0400

    40min

--- js/sandbox.js
+++ js/sandbox.js
@@ -1,9 +1,64 @@
-let age = 25;
-let year = 2019;
+// let age = 25;
+//let year = 2019;
 
-console.log(age, year);
+//console.log(age, year);
 
-age = 30;
-console.log(age);
+//age = 30;
+//console.log(age);
 
-const points = 100;
+//const points = 100;
+//points = 50;
+//console.log(points);
+
+//var score = 75;
+//console.log(score); 
+
+// strings
+//console.log('hello, world');
+
+//let email = '126evalentinnunez@frhsd.com';
+//console.log(email);
+
+// string concatenation
+//let firstName = 'Edwin';
+//let lastName = 'Valentin';
+
+//let fullName = firstName + ' ' + lastName;
+//console.log(fullName);
+
+// getting characters
+//console.log(fullName[2]);
+
+// string length
+//console.log(fullName.length);
+
+// string methods
+//console.log(fullName.toUpperCase());
+//let result = fullName.toLowerCase();
+//console.log(result, fullName);
+
+//let index = email.indexOf('@');
+//console.log(index);
+
+// common string methods
+
+//let email = '126evalentinnunez@frhsd.com';
+
+//let result = email.lastIndexOf('n');
+
+//let result = email.slice(0,5);
+
+//let result = email.substr(4,10);
+
+//let result = email.replace('m', 'w');
+
+//let result = email.replace('n', 'w');
+
+//console.log(result);
+
+let radius = 10;
+const pi = 3.14;
+
+//console.log(radius, pi)
+
+//
